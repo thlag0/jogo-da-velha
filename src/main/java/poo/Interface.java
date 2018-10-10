49b833903326763f29846a75f3b847ceea5e2089
thlag0 <thiagouriques@gmail.com>
2018-10-10 09:04:21 -0300

    App - Parte 1

---
+++ src/main/java/poo/Interface.java
@@ -0,0 +1,46 @@
+package poo;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Interface {
+    private JPanel painelPrincipal;
+    private JLabel Q11;
+    private JLabel Q32;
+    private JLabel Q22;
+    private JLabel Q23;
+    private JLabel Q13;
+    private JLabel Q33;
+    private JLabel Q21;
+    private JLabel Q31;
+    private JLabel Q12;
+    private JLabel jogador;
+
+    public Interface() {
+        Q11.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);// aqui
+                 if(Q11.getText().equals("X")) {
+                     jogador.setText("Jogador 1");
+                     Q11.setText("O");
+                 }else {
+                     Q11.setText("X");
+                     jogador.setText("Jogador 2");
+
+                 }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Interface");
+        frame.setContentPane(new Interface().painelPrincipal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        frame.setLocationRelativeTo(null);
+    }
+}
